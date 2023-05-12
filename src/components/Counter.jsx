eaa7dcd1d2a9911504b94c09cc4677dc3948535b
dejourford <dejourford@my.unt.edu>
2023-05-12 10:47:01 -0500

    style buttons and add content to counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import "./counter.css"
+
+export default function Counter() {
+    return (
+        <div className="counter">
+            
+            <div className="counter-buttons">
+                <h1>0</h1>
+                <button className="plus-Btn">+</button>
+                <button className="minus-Btn">-</button>
+            </div>
+        </div>
+    )
+}
