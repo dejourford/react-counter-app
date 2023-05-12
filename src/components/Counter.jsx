400eb4ff311825c1670d6ec535d688f608fb230e
dejourford <dejourford@my.unt.edu>
2023-05-12 11:44:19 -0500

    create set count function for buttons

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,22 +1,32 @@
+import React from "react"
+import { useState } from "react"
 import "./counter.css"
 
+
+
 export default function Counter() {
-    const handleMinusClick = () => {
-        console.log('minus')
+    const [count, setCount] = React.useState(0)
+
+    const add = () => {
+        console.log('add')
+        setCount(count + 1)
+        
     }
 
-    const handlePlusClick = () => {
-        console.log('plus')
+    const subtract = () => {
+        console.log('minus')
+        setCount(count - 1)
     }
     
     
+
     return (
         <div className="counter">
             
             <div className="counter-buttons">
-                <h1>0</h1>
-                <button className="plus-Btn" onClick={handlePlusClick}>+</button>
-                <button className="minus-Btn" onClick={handleMinusClick}>-</button>
+                <h1>{count}</h1>
+                <button className="plus-Btn" onClick={add}>+</button>
+                <button className="minus-Btn" onClick={subtract}>-</button>
             </div>
         </div>
     )
