a0e793c378c67437395256046a2ec95c1c64d000
dejourford <dejourford@my.unt.edu>
2023-05-12 11:32:31 -0500

    create function for on click

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,22 @@
 import "./counter.css"
 
 export default function Counter() {
+    const handleMinusClick = () => {
+        console.log('minus')
+    }
+
+    const handlePlusClick = () => {
+        console.log('plus')
+    }
+    
+    
     return (
         <div className="counter">
             
             <div className="counter-buttons">
                 <h1>0</h1>
-                <button className="plus-Btn">+</button>
-                <button className="minus-Btn">-</button>
+                <button className="plus-Btn" onClick={handlePlusClick}>+</button>
+                <button className="minus-Btn" onClick={handleMinusClick}>-</button>
             </div>
         </div>
     )
